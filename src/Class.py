ac15bd1366d8739fb742db5605be79538e00e980
MoOrabi <informatikerm@gmail.com>
2023-12-15 11:07:30 +0200

    Classes Initialized

---
+++ src/Class.py
@@ -0,0 +1,7 @@
+class SubjectClass:
+    def __init__(self, course, faculty_member, class_type, date, time):
+        self.time = time
+        self.date = date
+        self.class_type = class_type
+        self.faculty_member = faculty_member
+        self.course = course
---
+++ src/main.py
@@ -0,0 +1,22 @@
+from src.Course import Course
+
+levels_courses = []
+professors = ["Mohammed Ali", "Ahmed Khaled", "Ismail Osama"]
+assistants = ["Ali Mohammed", "Khaled Ahmed", "Osama Ismail"]
+
+level_1_courses = [Course("Math1", "101",
+                          2, 2, 1,
+                          ["Mohammed Ali", "Ahmed Khaled", "Ismail Osama"],
+                          ["Ali Mohammed", "Khaled Ahmed", "Osama Ismail"])]
+levels_courses.append(level_1_courses)
+
+level_2_courses = []
+levels_courses.append(level_2_courses)
+
+level_3_courses = []
+levels_courses.append(level_2_courses)
+
+level_4_courses = []
+levels_courses.append(level_2_courses)
+
+levels_classes = []
