c03e2555f14af88ff8e6cabd54be79f8d64ec60a
Abdo <abdo@gmail.com>
2023-12-16 08:46:00 +0200

    adding class(level_) updating file data_

--- src/data_.py
+++ src/data_.py
@@ -1,5 +1,6 @@
 import professor_ as p
 import course_ as c
+import level as l
 
 full_table = {}
 
@@ -29,29 +30,32 @@ assistants = [p.Professor("13", "Ahmed Mohamed",   "101", "301", "301"),
               p.Professor("23", "Karim Mohamed",  "211", "405", "301"),
               p.Professor("24", "Mamdouh Adel",   "212", "406", "301")]
 
-courses = {"level_1": [c.Course("101", "Introduction to Programming",    2, 2, 1),
+courses = {"1": [c.Course("101", "Introduction to Programming",    2, 2, 1),
                        c.Course("102", "Data Structures and Algorithms", 2, 2, 1),
                        c.Course("103", "Database Management Systems",    2, 2, 1),
                        c.Course("104", "Computer Networks",              2, 2, 1),
                        c.Course("105", "Operating Systems",              2, 2, 1),
                        c.Course("106", "Software Engineering",           2, 2, 1)],
-           "level_2": [c.Course("207", "Cybersecurity Basics",           2, 2, 1),
+           "2": [c.Course("207", "Cybersecurity Basics",           2, 2, 1),
                        c.Course("208", "Web Development Fundamentals",   2, 2, 1),
                        c.Course("209", "Artificial Intelligence Concepts", 2, 2, 1),
                        c.Course("210", "Machine Learning Applications",  2, 2, 1),
                        c.Course("211", "Data Science Essentials",        2, 2, 1),
                        c.Course("212", "Network Security Protocols",     2, 2, 1)],
-           "level_3": [c.Course("301", "Introduction to Cryptography",   2, 2, 1),
+           "3": [c.Course("301", "Introduction to Cryptography",   2, 2, 1),
                        c.Course("302", "Web Development Advanced",       2, 2, 1),
                        c.Course("303", "Advanced AI Concepts",           2, 2, 1),
                        c.Course("304", "Advanced Machine Learning",      2, 2, 1),
                        c.Course("305", "Advanced Data Science",          2, 2, 1),
                        c.Course("306", "Advanced Network Security",      2, 2, 1)],
-           "level_4": [c.Course("401", "Advanced Cryptography",          2, 2, 1),
+           "4": [c.Course("401", "Advanced Cryptography",          2, 2, 1),
                        c.Course("402", "Cloud Computing",                2, 2, 1),
                        c.Course("403", "Computer Vision",                2, 2, 1),
                        c.Course("404", "Advanced Robotics",              2, 2, 1),
                        c.Course("405", "Natural Language Processing",    2, 2, 1),
                        c.Course("406", "Cyber-Physical Systems",         2, 2, 1)]}
 
-
+levels = [l.level("1", 2),
+          l.level("2", 2),
+          l.level("3", 2),
+          l.level("4", 2)]
---
+++ src/level_.py
@@ -0,0 +1,4 @@
+class Level:
+    def __init__(self, level_num, num_of_sections):
+        self.levelnum = level_num
+        self.num_of_sections = num_of_sections
