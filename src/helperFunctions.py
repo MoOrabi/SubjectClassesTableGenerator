205da5333022be54276fe1f8d0470c7f68f12abb
MoOrabi <informatikerm@gmail.com>
2023-12-16 06:38:54 +0200

    Classes distributed

--- src/helperFunctions.py
+++ src/helperFunctions.py
@@ -1,7 +1,7 @@
 from src.data import level1_times, level_1_courses, level1_classes, \
-                    level2_times,level_2_courses, level2_classes, \
-                    level3_times,level_3_courses, level3_classes, \
-                    level4_times,level_4_courses, level4_classes
+                     level2_times, level_2_courses, level2_classes, \
+                     level3_times, level_3_courses, level3_classes, \
+                     level4_times, level_4_courses, level4_classes
 
 
 def choose_time_for_class(level_times):
