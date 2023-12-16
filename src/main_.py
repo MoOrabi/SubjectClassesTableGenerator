038218c0eff3fc6413bf98eb3c146cdcb3efb717
Abdo <abdo@gmail.com>
2023-12-16 16:06:58 +0200

    main file can create the table fully(الحمد لله)

--- src/main_.py
+++ src/main_.py
@@ -1,6 +1,7 @@
 import data_ as d
 import course_ as c
 from random import randint
+from pprint import pprint
 
 
 def get_course(courses_list, course_code):
@@ -30,13 +31,32 @@ def get_level_courses(courses_list, level_num):
 
 def add_class(course_code, day, slot, class_type):
     # day, slot should be numbers
-    d.full_table[int(course_code[0])][days[day]][slots[slot]] = [course_code, class_type]
+    d.full_table[int(course_code[0])][d.days[day]][d.slots[slot]] = [course_code, class_type]
+
 
 def check_table_free(level, day, slot):
-    if d.full_table[level][days[day]][slots[slot]] == None:
+    if d.full_table[level][d.days[day]][d.slots[slot]] == None:
         return True
     else:
         return False
+
+
+def assign_classes_to_courses(level, class_type):
+    level_courses = get_level_courses(d.courses, level)
+    for course in level_courses:
+        if class_type == "lecture":
+            ctr = course.get_no_lectures()
+        elif class_type == "section":
+            ctr = course.get_no_sections()
+        elif class_type == "lab":
+            ctr = course.get_no_labs()
+        while ctr != 0:
+            day = randint(1, 6)
+            slot = randint(1, 5)
+            if check_table_free(level, day, slot):
+                add_class(course.get_code(), day, slot, class_type)
+                ctr -= 1
+
 
 # ************************* Start main *************************
 
@@ -74,13 +94,23 @@ while not check_all_courses_assigned_assis(d.courses):
                 else:
                     print("Can't find courses for assistant", assistant)
 
-# assign courses of level_1 to its table
-level_1_courses = get_level_courses(d.courses, 1)
-for course in level_1_courses:
-    day = randint(1, 6)
-    slot = randint(1, 5)
-    # while()
+# assign lectures to all levels
+assign_classes_to_courses(1, "lecture")
+assign_classes_to_courses(2, "lecture")
+assign_classes_to_courses(3, "lecture")
+assign_classes_to_courses(4, "lecture")
 
+# assign sections to all levels
+assign_classes_to_courses(1, "section")
+assign_classes_to_courses(2, "section")
+assign_classes_to_courses(3, "section")
+assign_classes_to_courses(4, "section")
+
+# assign labs to all levels
+assign_classes_to_courses(1, "lab")
+assign_classes_to_courses(2, "lab")
+assign_classes_to_courses(3, "lab")
+assign_classes_to_courses(4, "lab")
 
 
 
@@ -90,4 +120,5 @@ for course in level_1_courses:
 for course in d.courses:
     print(course)
 print("*" * 40)
+pprint(d.full_table)
 
